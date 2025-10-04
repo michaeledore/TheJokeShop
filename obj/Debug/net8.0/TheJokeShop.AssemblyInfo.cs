2ce0676f70d2311ee9a0abeb863b66f2270fc45c
stanley edore <edorestanley@gmail.com>
2025-10-03 22:48:27 -0300

    added the git ignore file

--- obj/Debug/net8.0/TheJokeShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/TheJokeShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheJokeShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b674f65a1fcb5a177e3feabad2f1901503985cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheJokeShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheJokeShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
